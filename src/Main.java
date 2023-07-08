90dfaae30498be14f9dfa2e3cd3299bb3ae6d3ce
yslamgulypirgulyyev <islamguly28@gmail.com>
2023-07-08 14:30:20 +0200

    remove methods of ll and leave only initialisation

--- src/Main.java
+++ src/Main.java
@@ -5,20 +5,9 @@ import linkedlist.LinkedList;
 public class Main {
     public static void main(String[] args) {
         LinkedList linkedList = new LinkedList(4);
-        linkedList.append(5);
-        linkedList.append(5);
-        System.out.println("Head: " + linkedList.getHead());
-        System.out.println("Tail: "+linkedList.getTail());
-        System.out.println("Length: " + linkedList.getLength());
-
-        linkedList.prepend(9);
-
-        linkedList.set(1,6);
-        linkedList.insert(0,10);
-        linkedList.remove(2);
-
-        linkedList.reverse();
         System.out.println("Linked list: ");
         linkedList.printList();
+
+
     }
 }

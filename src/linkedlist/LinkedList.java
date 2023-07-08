76ac0bdcdab2bd9a873db0a3c8fc0ce1ab85ca26
yslamgulypirgulyyev <islamguly28@gmail.com>
2023-07-08 14:04:40 +0200

    correct unnecessary assignment

--- src/linkedlist/LinkedList.java
+++ src/linkedlist/LinkedList.java
@@ -163,9 +163,9 @@ public class LinkedList {
         Node temp = head;
         head = tail;
         tail = temp;
-        Node after = temp.next;
+        Node after;
         Node before = null;
-        for (int i = 0; i < length; i++){
+        for(int i = 0; i < length; i++){
             after = temp.next;
             temp.next = before;
             before = temp;
